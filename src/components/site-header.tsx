f24b2a8f6ceb87127a6a36bfd6dc79c684ba59b5
DemoAlbum <84697704+DemoAlbum@users.noreply.github.com>
2026-09-17 17:02:50 +0800

    Update site-header.tsx

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -179,7 +179,7 @@ export function SiteHeader({
               aria-label={t("admin")}
               title={t("admin")}
               onClick={() => {
-                window.location.assign("/admin");
+                window.location.assign("/yYAmg3pzQzfY");
               }}
             >
               <CircleUserRound />
